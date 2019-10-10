ba5eafd16a80a68d85fca91edadaf7a5fab56e66
LarrySimiyu <larry.simiyu@gmail.com>
2019-10-10 12:15:44 -0700

    server set up adjusted

--- api/middleware/serversetup.js
+++ api/middleware/serversetup.js
@@ -4,10 +4,10 @@ const cors = require("cors");
 const morgan = require("morgan");
 // rouuters
 
-const userRouter = require("../routes/user-router");
+//const userRouter = require("../routes/user-router");
 const filmInfoRouter = require("../routes/filmInfo-router");
-const hitListRouter = require("../routes/hitList-router");
-const screenplayImgRouter = require("../routes/screenplayImage-router");
+//const hitListRouter = require("../routes/hitList-router");
+//const screenplayImgRouter = require("../routes/screenplayImage-router");
 
 // exports
 module.exports = server => {
@@ -15,8 +15,8 @@ module.exports = server => {
   server.use(express.json());
   server.use(cors());
   server.use(morgan("dev"));
-  server.use("/api/user", userRouter);
-  server.use("/api/hitList", hitListRouter);
+  //server.use("/api/user", userRouter);
+  //server.use("/api/hitList", hitListRouter);
   server.use("/api/filmInfo", filmInfoRouter);
-  server.use("/api/screenplayImage", screenplayImgRouter);
+  //server.use("/api/screenplayImage", screenplayImgRouter);
 };
