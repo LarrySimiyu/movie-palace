709d35c736d075b7f70157430acb2efa9685f16e
LarrySimiyu <larry.simiyu@gmail.com>
2019-10-10 11:55:15 -0700

    file set up

---
+++ index.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+
+const server = require("./api/server.js");
+
+const port = process.env.PORT || 6666;
+server.listen(port, () => console.log(`server on port ${port}`));
