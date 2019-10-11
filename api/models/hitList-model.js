ecdc4ab5128f951c455336738607ded40643ab0f
LarrySimiyu <larry.simiyu@gmail.com>
2019-10-11 14:28:07 -0700

    db added hitlist router added

--- api/models/hitList-model.js
+++ api/models/hitList-model.js
@@ -1,46 +1,48 @@
-const db = require("../../data/dbConfig");
+const db = require("../../data/dbConfig.js");
 
 module.exports = {
   add,
-  get,
   find,
+  get,
+  findById,
   remove,
-  update,
-  findById
+  update
 };
 
 async function add(body) {
-  return db("user").insert(body);
+  return db("hit_list").insert(body);
 }
 
 function find() {
-  return db("user");
+  return db("hit_list");
 }
 
 function get(id) {
   if (id) {
-    return db("user")
+    return db("hit_list")
       .where({ id: Number(id) })
       .first();
   } else {
-    return db("user");
+    return db("hit_list");
   }
 }
 
 function findById(id) {
-  return db("user")
+  return db("hit_list")
     .where({ id })
-    .first();
+    .first(); // find the first instance of this
 }
 
+// remove hitlist by id
 function remove(id) {
-  return db("user")
+  return db("hit_list")
     .where({ id })
     .del();
 }
 
+//update not defined
 function update(id, changes) {
-  return db("user")
+  return db("hit_list")
     .where({ id })
     .update(changes);
 }
